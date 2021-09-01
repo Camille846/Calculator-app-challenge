e01998e36ebbd5d8c9eda4b2810cb93435f8f93c
Camille <camillegomes74@gmail.com>
2021-08-31 22:25:56 -0300

    Feat: Display the result of the chosen opperation by using conditionals

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 function calculate(type, value){
    if (type === 'action') { 
-       if(value === 'c'){
+    if(value === 'c'){
         document.getElementById("display").value = ''
-       }
-    
-       if (value === '+' || value === '-' || value === '*' || value === '/' || value === '.'){
+    }
+
+    if (value === '+' || value === '-' || value === '*' || value === '/' || value === '.'){
         document.getElementById("display").value += value 
-       }
+    }
+
+    if(value === '='){
+        var result = eval(document.getElementById("display").value) 
+        document.getElementById("display").value = result
+    }
+     
 
    } else if(type === 'value'){
        document.getElementById("display").value += value 
